122860bfb79f991177194697403e54a36b0e972f
Allan-Flac <felipe.zuuma@gmail.com>
2026-05-04 17:00:45 +0000

    Desafio Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,9 +9,9 @@ int main() {
     char estado1;
     char codigoCarta1 [4];
     char cidade1 [50];
-    int população1;
-    float area1;
-    float pib1;
+    int populacao1;
+    int area1;
+    int pib1;
     int pontosTuristicos1;
 
     // ======================================= //
@@ -19,79 +19,79 @@ int main() {
     char estado2;
     char codigoCarta2 [4];
     char cidade2 [50];
-    int população2;
-    float area2;
-    float pib2;
+    int populacao2;
+    int area2;
+    int pib2;
     int pontosTuristicos2;
   // Área para entrada de dados
-    printf ("====== Cadastro de Carta 1 =====/n");
+    printf ("\n====== Cadastro de Carta 1 =====\n");
 
     printf ("Estado [A-H]: ");
-    scanf ("%c", &estado1);
+    scanf (" %c", &estado1);
 
     printf ("Codigo da carta: ");
     scanf ("%s", codigoCarta1);
 
     printf ("Nome da cidade: ");
-    scanf ("[^/n]", cidade1);
+    scanf ("%s", cidade1);
 
     printf ("População: ");
-    scanf ("%d", &população1);
+    scanf ("%d", &populacao1);
 
     printf ("Area em KM²: ");
-    scanf ("%f", &area1);
+    scanf ("%d", &area1);
 
     printf ("PIB: ");
-    scanf ("%f", &pib1);
+    scanf ("%d", &pib1);
 
     printf ("Numero de pontos turisticos: ");
     scanf ("%i", &pontosTuristicos1);
 
     // ========================================= //
 
-    printf ("====== Cadastro de carta 2 ======/n");
+    printf ("\n====== Cadastro de carta 2 ======\n");
 
     printf ("Estado [A-H]: ");
-    scanf ("%c", &estado2);
+    scanf (" %c", &estado2);
 
     printf ("Codigo da carta: ");
     scanf ("%s", codigoCarta2);
 
     printf ("Nome da cidade: ");
-    scanf ("[^/n]", cidade2);
+    scanf (" %s", cidade2);
 
     printf ("População: ");
-    scanf ("%d", &população2);
+    scanf ("%d", &populacao2);
 
     printf ("Area em KM²: ");
-    scanf ("%f", &area2);
+    scanf ("%d", &area2);
 
     printf ("PIB: ");
-    scanf ("%f", &pib2);
+    scanf ("%d", &pib2);
 
     printf ("Numero de pontos turisticos: ");
     scanf ("%i", &pontosTuristicos2);
   // Área para exibição dos dados da cidade
-  printf ("====== Cartas Cadastradas ======");
+  printf ("====== Cartas Cadastradas ======\n");
 
-  printf ("/nCarta 1:/n");
-  printf ("Estado: %c/n", estado1);
-  printf ("Codigo: %s/n", codigoCarta1);
-  printf ("Cidade: %s/n", cidade1);
-  printf ("População: %d/n", população1);
-  printf ("Area em Km²: %f/n", area1);
-  printf ("PIB: %f/n", pib1);
-  printf ("Numero de pontos turisticos %i/n", pontosTuristicos1);
+  printf ("Carta 1:\n");
+  printf ("Estado: %c\n", estado1);
+  printf ("Codigo: %s\n", codigoCarta1);
+  printf ("Cidade: %s\n", cidade1);
+  printf ("População: %d\n", populacao1);
+  printf ("Area em Km²: %d\n", area1);
+  printf ("PIB: %d\n", pib1);
+  printf ("Numero de pontos turisticos %i\n", pontosTuristicos1);
 
   // ================================================================ //
 
-  printf ("/nCarta 2:/n");
-  printf ("Estado: %c/n", estado2);
-  printf ("Codigo: %s/n", codigoCarta2);
-  printf ("Cidade: %s/n", cidade2);
-  printf ("População: %d/n", população2);
-  printf ("Area em Km²: %f/n", area2);
-  printf ("PIB: %f/n", pib2);
-  printf ("Numero de pontos turisticos %i/n", pontosTuristicos2);
+  printf ("\nCarta 2:\n");
+  printf ("Estado: %c\n", estado2);
+  printf ("Codigo: %s\n", codigoCarta2);
+  printf ("Cidade: %s\n", cidade2);
+  printf ("População: %d\n", populacao2);
+  printf ("Area em Km²: %d\n", area2);
+  printf ("PIB: %d\n", pib2);
+  printf ("Numero de pontos turisticos %i\n", pontosTuristicos2);
 return 0;
 } 
